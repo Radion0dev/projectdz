785ebfe4cf9151941de79ed70c5f4f35f59a9b6e
Rodion0 <radion0-dev@mail.ru>
2025-07-06 20:32:06 +0700

    добавил вывод

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 let title ='Алтай';
 let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 75;
+let screenPrice = 40;
 let rollback = 55;
 let fullPrice = 70;
 let adaptive = true;
 
-alert("lesson JavaScript");
+let screensArray = screens.toLowerCase().split(",");
+
+
+
+// alert("lesson JavaScript");
 
 console.log("lesson JavaScript");
+
+
+console.log(typeof(title));
+
+console.log(typeof(fullPrice));
+
+console.log(typeof(adaptive));
+
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screens} ${screenPrice} рублей, Стоимость разработки сайта ${fullPrice} рублей`);
+
+
+
+console.log(screensArray);
+
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback/100)}`);
 
